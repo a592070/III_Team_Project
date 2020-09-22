eccd8eaaf188f29eb778afb8ffac27949bdf4c11
a592070 <48921549+a592070@users.noreply.github.com>
2020-09-22 14:16:40 +0800

    Delete Test.java

--- src/main/java/iii/teamproject/controller/initTable/Test.java
+++
@@ -1,9 +0,0 @@
-package iii.teamproject.controller.initTable;
-
-import java.io.IOException;
-
-public class Test {
-    public static void main(String[] args) throws IOException {
-        new DataInit("").getListDO();
-    }
-}
