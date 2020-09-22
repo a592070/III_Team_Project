d017ea6ab4f2964cf7a300bef7e0ecdceb8d6058
iring29 <929iring@gmail.com>
2020-09-22 15:12:21 +0800

    add libs

---
+++ src/controller/initTable/Test.java
@@ -0,0 +1,13 @@
+package controller.initTable;
+
+import java.io.IOException;
+import java.util.List;
+
+import pojo.AttractionsDO;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+    	List<AttractionsDO> list = new DataInit("https://gis.taiwan.net.tw/XMLReleaseALL_public/scenic_spot_C_f.json").getListDO();
+    	System.out.println(list);
+    }
+}
