cb144ccb66d34065a5e7871320007c3840d30c01
a592070 <a592070@gmail.com>
2020-09-22 21:22:41 +0800

    20200922 Init table

--- src/controller/initTable/Test.java
+++ src/controller/initTable/Test.java
@@ -1,9 +1,12 @@
 package controller.initTable;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        new DataInit("").getListDO();
+    public static void main(String[] args) throws IOException, SQLException {
+//        new AttractionsInit(AttractionsInit.Scenic_Spot_URL).initTable();
+//        new AttractionsInit(AttractionsInit.Restaurant_URL).initTable();
+//        new AttractionsInit(AttractionsInit.Hotel_URL).initTable();
     }
 }
