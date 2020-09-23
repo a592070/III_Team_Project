7f1e0c6b02cb3adc412280093969ef2c51fd78b1
iring29 <929iring@gmail.com>
2020-09-23 21:58:19 +0800

    9/23 pull data from master, merge into dev_iring29

--- src/controller/initTable/Test.java
+++ src/controller/initTable/Test.java
@@ -1,4 +1,5 @@
 package controller.initTable;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import utils.StringUtil;
@@ -9,11 +10,11 @@ import java.util.Iterator;
 
 public class Test {
     public static void main(String[] args) throws IOException, SQLException {
-//        new AttractionsInit(AttractionsInit.Scenic_Spot_URL).initTable();
-//        new AttractionsInit(AttractionsInit.Restaurant_URL).initTable();
-//        new AttractionsInit(AttractionsInit.Hotel_URL).initTable();
+        new AttractionsInit(AttractionsInit.Scenic_Spot_URL).initTable();
+        new AttractionsInit(AttractionsInit.Restaurant_URL).initTable();
+        new AttractionsInit(AttractionsInit.Hotel_URL).initTable();
 
-        String str = "{\"abc\":\"123\", \"def\":[\"1\", \"2\"], \"qwe\":{\"a\":[\"1\", \"2\"], \"b\":\"3\"}}";
+//        String str = "{\"abc\":\"123\", \"def\":[\"1\", \"2\"], \"qwe\":{\"a\":[\"1\", \"2\"], \"b\":\"3\"}}";
 
         /**
          * {
@@ -27,9 +28,9 @@ public class Test {
          *
           */
 
-        ObjectMapper mapper = new ObjectMapper();
-
-        JsonNode jsonNode = mapper.readTree(str);
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        JsonNode jsonNode = mapper.readTree(str);
 
 //        JsonNode node = jsonNode.path("qwe");
 //        System.out.println(node);
