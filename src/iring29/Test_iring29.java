a958aeba44e36ea504b46e52f4e6537c2a100b4e
iring29 <929iring@gmail.com>
2020-09-25 15:14:57 +0800

    9/25 pull from master

--- src/iring29/Test_iring29.java
+++ src/iring29/Test_iring29.java
@@ -1,25 +1,26 @@
 package iring29;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.sql.DataSource;
+
+import controller.ConnectionPool;
 import pojo.AccountDO;
 
 public class Test_iring29 {
-	public static void main(String[] args) throws IOException {
-//		HomepageInit test = new HomepageInit();
+	public static void main(String[] args) throws IOException, SQLException {
+
+//		HomepageInit hpInit = new HomepageInit();
 //		AccountDO accDo = new AccountDO();
-//		accDo.setUsername("test");
-//		System.out.println(accDo.getUsername());
-//		test.searchUsername(accDo);
-//		System.out.println(test.password);
+//		hpInit.searchUsername(accDo);
+//		System.out.println(hpInit.password);
+//		String param1 = hpInit.password;
+//		System.out.println(param1);
 		
-		HomepageInit hpInit = new HomepageInit();
-		AccountDO accDo = new AccountDO();
-		hpInit.searchUsername(accDo);
-		accDo.setUsername("test");
-		System.out.println(hpInit.password);
-		String param1 = hpInit.password;
-		System.out.println(param1);
+
 
 	}
 }
