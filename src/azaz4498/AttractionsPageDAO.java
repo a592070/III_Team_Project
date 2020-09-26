c9da36d099680446802ae1d51fc20fd0f28181e7
azaz4498 <azaz4498@gmail.com>
2020-09-26 20:23:36 +0800

    adding servlet and basic web frame

--- src/azaz4498/AttractionsPageDAO.java
+++ src/azaz4498/AttractionsPageDAO.java
@@ -2,17 +2,16 @@ package azaz4498;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import controller.ConnectionPool;
-import pojo.AttractionsDO;
 
 public class AttractionsPageDAO {
 	private Connection conn;
@@ -27,23 +26,31 @@ public class AttractionsPageDAO {
 	public void pageListInit() throws SQLException {
 		list = new ArrayList<>();
 		try {
-			String sql = "SELECT NAME,PX,PY,DESCRIPTION FROM attractions";
+			String sql = "SELECT * FROM attractions";
 			conn = ds.getConnection();
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			while (rs.next()) {
 				AttractionsPageVO attractionsPageVO = new AttractionsPageVO();
-				AttractionsPageVO.setName(rs.getString("name"));
-				AttractionsPageVO.setPx(rs.getBigDecimal("px"));
-				AttractionsPageVO.setPy(rs.getBigDecimal("py"));
-				AttractionsPageVO.setDescription(rs.getString("description"));
-
-				list.add(AttractionsPageVO);
 				
+				attractionsPageVO.setName(rs.getString("name"));
+				attractionsPageVO.setDescription(rs.getString("description"));
+				attractionsPageVO.setAddress(rs.getString("address"));
+				attractionsPageVO.setTel(rs.getString("tel"));
+				attractionsPageVO.setPx(rs.getBigDecimal("px"));
+				attractionsPageVO.setPy(rs.getBigDecimal("py"));
+				attractionsPageVO.setOpentime(rs.getString("opentime"));
+				attractionsPageVO.setTraveling_info(rs.getString("traveling_info"));
+				attractionsPageVO.setTotal_number_rooms(rs.getString("total_number_rooms"));
+				attractionsPageVO.setService_info(rs.getString("service_info"));
+				attractionsPageVO.setRating(rs.getBigDecimal("rating"));
+				attractionsPageVO.setRegion(rs.getString("region_name"));
+				
+				list.add(attractionsPageVO);
 				
 			}
-			//print測試是否取得資料庫資料
-			System.out.println(list);
+//			print測試是否取得資料庫資料
+//			System.out.println(list);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -54,17 +61,24 @@ public class AttractionsPageDAO {
 		}
 	}
 
-	public List<AttractionsDO> listAtrractionsPageDO() {
-		return list;
-	}
+	
 
 	public void addIntoList() throws SQLException {
 
 		try {
 			conn = ds.getConnection();
+			String sql = "INSERT INTO ACCOUNT (FAVORITE) VALUES('?') WHERE IDENTITY='?'";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1,"C1_376490000A_100130" );  //暫時使用第一筆資料作為加入清單依據，如何獲取當前頁面景點ID待解決。
+			pstmt.setString(2, "username");
+			pstmt.executeQuery();
 
 		} catch (SQLException e) {
 			// TODO: handle exception
+		}finally {
+			if (conn!=null) {
+				conn.close();
+			}
 		}
 
 	}
