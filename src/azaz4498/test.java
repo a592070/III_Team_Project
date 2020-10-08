9834904710fe765daad7e311c1528605bd2a12b1
azaz4498 <azaz4498@gmail.com>
2020-10-08 21:50:41 +0800

    add ArticleServlet.java, ForumServlet.java ,Forum.jsp ,Article.jsp

--- src/azaz4498/test.java
+++ src/azaz4498/test.java
@@ -2,15 +2,21 @@ package azaz4498;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Calendar;
 
 import controller.ConnectionPool;
 
 public class test {
 
 	public static void main(String[] args) throws IOException, SQLException{
-//		AttractionsPageDAO attractionsPageDAO = new AttractionsPageDAO(ConnectionPool.LOADING_WITHOUT_SERVER);
-//		attractionsPageDAO.pageListInit();
+		ForumDAO forumDAO = new ForumDAO(ConnectionPool.LOADING_WITHOUT_SERVER);
 		
+		
+		forumDAO.articleListInit();
+		System.out.println(forumDAO.artileList());
+		
+		
+//		System.out.println(new java.sql.Timestamp(Calendar.getInstance().getTime().getTime()));
 
 	}
 
