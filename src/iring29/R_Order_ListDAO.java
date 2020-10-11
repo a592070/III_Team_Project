59fa1d5e7d991544032342477b3569cfe5f3d75d
iring29 <929iring@gmail.com>
2020-10-11 16:27:30 +0800

    20201011 modify display order list portion

--- src/iring29/R_Order_ListDAO.java
+++ src/iring29/R_Order_ListDAO.java
@@ -29,7 +29,7 @@ public class R_Order_ListDAO {
 
 	// create order
 	public void createOrder(OrderTableBean otbean) throws SQLException {
-		
+
 		String sqlOrder_Table = "insert into ORDER_TABLE (ORDER_DATE, USERNAME) values(?, ?)";
 
 		String sqlR_Order_List = "insert into R_ORDER_LIST (ORDER_ID, R_SN, CUSTOMER_NUM, BOOK_TIME, DEPOSIT, CUS_NAME, CUS_PHONE) "
@@ -41,9 +41,8 @@ public class R_Order_ListDAO {
 			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sqlOrder_Table, generatedColumn1);
 			Timestamp ts = new Timestamp(System.currentTimeMillis()); // 下訂單時間
-			pstmt.setTimestamp(1, ts); 
+			pstmt.setTimestamp(1, ts);
 			pstmt.setString(2, otbean.getUser().getUserName()); // 取得userbean裡面的username
-		
 
 			pstmt.executeQuery();
 			pstmt.clearBatch();
@@ -55,12 +54,12 @@ public class R_Order_ListDAO {
 			} else {
 				throw new RuntimeException("無法取得新增之ORDER_TABLE表格的主鍵");
 			}
-			
+
 			Set<R_OrderBean> oBean = otbean.getR_OderBeans();
 			try {
 				PreparedStatement pstmt2 = conn.prepareStatement(sqlR_Order_List);
-				
-				for(R_OrderBean ele : oBean) {
+
+				for (R_OrderBean ele : oBean) {
 					pstmt2.setBigDecimal(1, order_id); // ORDER_ID from ORDER_TABLE
 					pstmt2.setBigDecimal(2, ele.getRestaurantBean().getR_sn());
 					pstmt2.setBigDecimal(3, ele.getCustomerNum());
@@ -69,9 +68,9 @@ public class R_Order_ListDAO {
 					pstmt2.setBigDecimal(5, ele.getDeposit()); // 訂餐廳訂金
 					pstmt2.setString(6, ele.getCustomerName());
 					pstmt2.setString(7, ele.getCustomerPhone());
-					
+
 				}
-				
+
 				pstmt2.executeQuery();
 
 			} catch (SQLException ex) {
@@ -80,7 +79,7 @@ public class R_Order_ListDAO {
 			}
 			conn.commit();
 			pstmt.close();
-			
+
 		} catch (Exception e) {
 			System.err.println("新增資料時發生錯誤:" + e);
 			conn.rollback();
@@ -94,39 +93,52 @@ public class R_Order_ListDAO {
 
 	// find r_order
 	public R_OrderBean findR_order_List(BigDecimal r_sn) throws SQLException {
-		String sql = "select r_sn_order, max(order_id) order_id , customer_num, book_time, cus_name, cus_phone from r_order_list "
-				+ "where r_sn = ? group by r_sn_order, customer_num, book_time, cus_name, cus_phone";
+		String sql1 = "select max(order_id) order_id from r_order_list where r_sn = ?";
+		String sql2 = "select * from r_order_list where order_id = ?";
+
 		try {
 			conn = ds.getConnection();
-			pstmt = conn.prepareStatement(sql);
+			pstmt = conn.prepareStatement(sql1);
 			pstmt.setBigDecimal(1, r_sn);
 			ResultSet rs = pstmt.executeQuery();
-			pstmt.clearBatch();
-			
-			OrderTableBean otBean = new OrderTableBean();
-			R_OrderBean roBean = new R_OrderBean();
+			BigDecimal id = null;
 			while (rs.next()) {
-				BigDecimal r_sn_order = rs.getBigDecimal("R_SN_ORDER");
-				BigDecimal order_id = rs.getBigDecimal("ORDER_ID");
-				BigDecimal c_num = rs.getBigDecimal("CUSTOMER_NUM");
-				Timestamp b_time = rs.getTimestamp("BOOK_TIME");
-				String cus_name = rs.getString("CUS_NAME");
-				String cus_phone = rs.getString("CUS_PHONE");
-				roBean.setR_sn_order(r_sn_order);
-				roBean.setOrder_id(order_id);
-				roBean.setCustomerNum(c_num);
-				roBean.setBooking_date(b_time);
-				roBean.setCustomerName(cus_name);
-				roBean.setCustomerPhone(cus_phone);
-				otBean.addR_OderBean(roBean);
+				id = rs.getBigDecimal("ORDER_ID");
 			}
-			return roBean;
+			pstmt.clearBatch();
 
+			try {
+				pstmt = conn.prepareStatement(sql2);
+				pstmt.setBigDecimal(1, id);
+				ResultSet rs2 = pstmt.executeQuery();
+				pstmt.clearBatch();
+
+				OrderTableBean otBean = new OrderTableBean();
+				R_OrderBean roBean = new R_OrderBean();
+				while (rs2.next()) {
+					BigDecimal r_sn_order = rs2.getBigDecimal("R_SN_ORDER");
+					BigDecimal order_id = rs2.getBigDecimal("ORDER_ID");
+					BigDecimal c_num = rs2.getBigDecimal("CUSTOMER_NUM");
+					Timestamp b_time = rs2.getTimestamp("BOOK_TIME");
+					String cus_name = rs2.getString("CUS_NAME");
+					String cus_phone = rs2.getString("CUS_PHONE");
+					roBean.setR_sn_order(r_sn_order);
+					roBean.setOrder_id(order_id);
+					roBean.setCustomerNum(c_num);
+					roBean.setBooking_date(b_time);
+					roBean.setCustomerName(cus_name);
+					roBean.setCustomerPhone(cus_phone);
+					otBean.addR_OderBean(roBean);
+				}
+				return roBean;
+
+			} catch (Exception e) {
+				System.err.println("查詢資料時發生錯誤:" + e);
+				return null;
+			}
 		} catch (Exception e) {
-			System.err.println("查詢資料時發生錯誤:" + e);
-			conn.rollback();
+			System.err.println("新增資料時發生錯誤:" + e);
 			return null;
-
 		} finally {
 			if (conn != null) {
 				conn.close();
@@ -136,7 +148,7 @@ public class R_Order_ListDAO {
 	}
 
 	// Delete Order
-	public boolean cancelR_Order(BigDecimal r_sn_order )throws SQLException {
+	public boolean cancelR_Order(BigDecimal r_sn_order) throws SQLException {
 		String sql = "delete r_order_list where r_sn_order = ?";
 		try {
 			conn = ds.getConnection();
@@ -169,8 +181,8 @@ public class R_Order_ListDAO {
 			pstmt.setBigDecimal(1, r_sn);
 			ResultSet rs = pstmt.executeQuery();
 			pstmt.clearBatch();
-			
-			 OrderTableBean otBean = new OrderTableBean();
+
+			OrderTableBean otBean = new OrderTableBean();
 
 			while (rs.next()) {
 				BigDecimal r_sn_order = rs.getBigDecimal("R_SN_ORDER");
@@ -180,7 +192,8 @@ public class R_Order_ListDAO {
 				BigDecimal deposit = rs.getBigDecimal("DEPOSIT");
 				String cus_name = rs.getString("CUS_NAME");
 				String cus_phone = rs.getString("CUS_PHONE");
-				R_OrderBean roBean = new R_OrderBean(r_sn_order, orderID, ts, cus_num, deposit, null,cus_name,cus_phone);
+				R_OrderBean roBean = new R_OrderBean(r_sn_order, orderID, ts, cus_num, deposit, null, cus_name,
+						cus_phone);
 				otBean.addR_OderBean(roBean);
 			}
 			rs.close();
@@ -198,38 +211,37 @@ public class R_Order_ListDAO {
 			}
 		}
 	}
-	
-	
-	//find booking name, phone
-			public OrderTableBean BookData(BigDecimal order_id) throws SQLException {
-				String sql = "select name, phone from order_data where sn = (select sn_no from order_table where order_id = ?)";
-				
-				try {
-					conn = ds.getConnection();
-					pstmt = conn.prepareStatement(sql);
-					pstmt.setBigDecimal(1, order_id);
-					ResultSet rs = pstmt.executeQuery();
-					pstmt.clearBatch();
-					OrderTableBean otBean = new OrderTableBean();
-					while (rs.next()) {
-						
+
+	// find booking name, phone
+	public OrderTableBean BookData(BigDecimal order_id) throws SQLException {
+		String sql = "select name, phone from order_data where sn = (select sn_no from order_table where order_id = ?)";
+
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setBigDecimal(1, order_id);
+			ResultSet rs = pstmt.executeQuery();
+			pstmt.clearBatch();
+			OrderTableBean otBean = new OrderTableBean();
+			while (rs.next()) {
+
 //						otBean.setCustomerName(rs.getString("NAME"));
 //						otBean.setCustomerPhone(rs.getString("PHONE"));
-						
-					}
-					rs.close();
-					pstmt.close();
-					return otBean;
-				}catch (Exception e) {
-					System.err.println("新增資料時發生錯誤:" + e);
-					conn.rollback();
-					return null;
 
-				} finally {
-					if (conn != null) {
-						conn.close();
-					}
-				}
-				
 			}
+			rs.close();
+			pstmt.close();
+			return otBean;
+		} catch (Exception e) {
+			System.err.println("新增資料時發生錯誤:" + e);
+			conn.rollback();
+			return null;
+
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+
+	}
 }
