7bab42bedaedd8bfe65957a2dba87a1d65312e32
innocence741 <innocence741@gmail.com>
2020-10-12 09:34:32 +0800

    AccountBean改回去

--- src/rambo0021/AccountBean.java
+++ src/rambo0021/AccountBean.java
@@ -15,9 +15,9 @@ public class AccountBean {
 	private int identity;
 	private String email;
 	private Blob picture;
-	private Date modify_Date = new Date();
+	private Date modify_Date;
 	private String nickName;
-	private Date register = new Date();
+	private Date register;
 	private String identityString;
 	
 	public AccountBean() {
