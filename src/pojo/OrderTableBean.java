3ed1b93fdd1d21d1c07fb5686b496f1d8cb8b9ec
innocence741 <innocence741@gmail.com>
2020-10-23 15:46:35 +0800

    更改OrderTable交通部分

--- src/pojo/OrderTableBean.java
+++ src/pojo/OrderTableBean.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 import asx54630.H_OrderBean;
 import innocence741.T_OrderBean;
@@ -26,22 +27,21 @@ public class OrderTableBean {
 	@Column(name = "ORDER_ID")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	BigDecimal order_id; // PK
-//	BigDecimal price;		// H + C + R  H:Hotel; C:Car; R:Restaurant
 
 	@Column(name = "ORDER_DATE")
 	Timestamp order_date; // 下訂單時間，default
 
 //	@						//還沒註冊
 	AccountBean user; // Account資料
-//	String customerName; 	//下單時填入的姓名
-//	String customerPhone;	//下單時填入的電話
+
 
 	Set<R_OrderBean> r_OrderBeans;
 	R_OrderBean r_OdrerBean; // 小訂單的Bean (Restaurant)
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "order_table", cascade = CascadeType.ALL)
 	Set<T_Order_List> t_OrderBeans;
-//	T_OrderBean T_OderBean;  //小訂單的Bean (Traffic)
+	@Transient
+	T_OrderBean T_OderBean;  //小訂單的Bean (Traffic)
 	H_OrderBean h_OderBean; // 小訂單的Bean (hotel)
 	Set<H_OrderBean> h_OrderBeans;
 
@@ -51,7 +51,7 @@ public class OrderTableBean {
 		t_OrderBeans = new HashSet<T_Order_List>();
 
 	}
-
+	
 	public BigDecimal getOrder_id() {
 		return order_id;
 	}
@@ -59,6 +59,7 @@ public class OrderTableBean {
 	public void setOrder_id(BigDecimal order_id) {
 		this.order_id = order_id;
 	}
+
 
 	public Timestamp getOrder_date() {
 		return order_date;
@@ -76,21 +77,50 @@ public class OrderTableBean {
 		this.user = user;
 	}
 
-	public Set<R_OrderBean> getR_OrderBeans() {
+//	public String getCustomerName() {
+//		return customerName;
+//	}
+//
+//	public void setCustomerName(String customerName) {
+//		this.customerName = customerName;
+//	}
+//
+//	public String getCustomerPhone() {
+//		return customerPhone;
+//	}
+//
+//	public void setCustomerPhone(String customerPhone) {
+//		this.customerPhone = customerPhone;
+//	}
+
+	public Set<R_OrderBean> getR_OderBeans() {
 		return r_OrderBeans;
 	}
+	
 
-	public void setR_OrderBeans(Set<R_OrderBean> r_OrderBeans) {
-		this.r_OrderBeans = r_OrderBeans;
+
+	public void setR_OderBeans(Set<R_OrderBean> r_OderBeans) {
+		this.r_OrderBeans = r_OderBeans;
 	}
 
-	public R_OrderBean getR_OdrerBean() {
-		return r_OdrerBean;
+	public void addR_OderBean(R_OrderBean rBean) {
+		this.r_OrderBeans.add(rBean);
 	}
 
-	public void setR_OdrerBean(R_OrderBean r_OdrerBean) {
-		this.r_OdrerBean = r_OdrerBean;
+	
+
+	public Set<H_OrderBean> getH_OrderBeans() {
+		return h_OrderBeans;
 	}
+
+	public void setH_OrderBeans(Set<H_OrderBean> h_OrderBeans) {
+		this.h_OrderBeans = h_OrderBeans;
+	}
+
+	public void addH_OrderBean(H_OrderBean hBean) {
+		this.h_OrderBeans.add(hBean);
+	}
+	
 
 	public Set<T_Order_List> getT_OrderBeans() {
 		return t_OrderBeans;
@@ -100,20 +130,8 @@ public class OrderTableBean {
 		this.t_OrderBeans = t_OrderBeans;
 	}
 
-	public H_OrderBean getH_OderBean() {
-		return h_OderBean;
-	}
-
-	public void setH_OderBean(H_OrderBean h_OderBean) {
-		this.h_OderBean = h_OderBean;
-	}
-
-	public Set<H_OrderBean> getH_OrderBeans() {
-		return h_OrderBeans;
-	}
-
-	public void setH_OrderBeans(Set<H_OrderBean> h_OrderBeans) {
-		this.h_OrderBeans = h_OrderBeans;
+	public void addT_OderBean(T_Order_List tBean) {
+		this.t_OrderBeans.add(tBean);
 	}
 
 }
