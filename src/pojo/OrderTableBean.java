399b93fbfb7a94a287510656111056eda5f2dc8a
innocence741 <innocence741@gmail.com>
2020-10-26 15:14:55 +0800

    把被註解的東西打開

--- src/pojo/OrderTableBean.java
+++ src/pojo/OrderTableBean.java
@@ -35,10 +35,10 @@ public class OrderTableBean {
 	@Transient
 	R_OrderBean r_OrdrerBean; // 小訂單的Bean (Restaurant)
 
-//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "order_table", cascade = CascadeType.ALL)
-//	Set<T_Order_List> t_Order_Lists;
-//	@Transient
-//	T_Order_List t_Order_List;  //小訂單的Bean (Traffic)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "order_table", cascade = CascadeType.ALL)
+	Set<T_Order_List> t_Order_Lists;
+	@Transient
+	T_Order_List t_Order_List;  //小訂單的Bean (Traffic)
 	
 //	H_OrderBean h_OderBean; // 小訂單的Bean (hotel)
 //	Set<H_OrderBean> h_OrderBeans;
@@ -120,16 +120,16 @@ public class OrderTableBean {
 //	}
 //	
 //
-//	public Set<T_Order_List> getT_Order_Lists() {
-//		return t_Order_Lists;
-//	}
-//
-//	public void setT_Order_Lists(Set<T_Order_List> t_OrderBeans) {
-//		this.t_Order_Lists = t_OrderBeans;
-//	}
-//
-//	public void addT_Order_Lists(T_Order_List tBean) {
-//		this.t_Order_Lists.add(tBean);
-//	}
+	public Set<T_Order_List> getT_Order_Lists() {
+		return t_Order_Lists;
+	}
+
+	public void setT_Order_Lists(Set<T_Order_List> t_OrderBeans) {
+		this.t_Order_Lists = t_OrderBeans;
+	}
+
+	public void addT_Order_Lists(T_Order_List tBean) {
+		this.t_Order_Lists.add(tBean);
+	}
 
 }
