9ff76679cff168aefe36805800560d648d9aa233
a592070 <a592070@gmail.com>
2020-10-27 18:35:39 +0800

    20201027 check commit

--- src/a592070/servlet/AttractionsInfoServlet.java
+++ src/a592070/servlet/AttractionsInfoServlet.java
@@ -98,42 +98,4 @@ public class AttractionsInfoServlet extends HttpServlet {
         mapper.writeValue(resp.getWriter(), objectNode);
 
     }
-//    public void selectKeyword(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-//        try {
-//            ObjectMapper mapper = new ObjectMapper();
-//            ObjectNode objectNode = mapper.createObjectNode();
-//
-//            String currentPage = req.getParameter("nowPage");
-//            String keyword = req.getParameter("keyword");
-//            System.out.println(currentPage);
-//            System.out.println(keyword);
-//
-//            PageSupport pageSupport = new PageSupport();
-//            pageSupport.setPageSize(pageSize);
-//
-//            if (!StringUtil.isEmpty(keyword)) {
-//                pageSupport.setTotalCount(attractionService.getKeywordLimitSize(keyword));
-//            } else {
-//                pageSupport.setTotalCount(attractionService.getTotalSize());
-//            }
-//
-//            if (!StringUtil.isEmpty(currentPage)) {
-//                pageSupport.setCurrentPage(Integer.parseInt(currentPage));
-//            }
-//
-//            int currentPageNo = pageSupport.getCurrentPage();
-//
-//
-//            List<AttractionDO> list = attractionService.searchEle(currentPageNo, pageSupport.getPageSize(), keyword);
-//
-//
-//            objectNode.put("currentPage", currentPageNo);
-//            objectNode.put("totalPage", pageSupport.getTotalPageCount());
-//            objectNode.put("totalCount", pageSupport.getTotalCount());
-//            objectNode.put("currentPageList", mapper.writeValueAsString(list));
-//            mapper.writeValue(resp.getWriter(), objectNode);
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
